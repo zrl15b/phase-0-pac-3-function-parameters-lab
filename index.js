ee5f15b681473842c0548bc08ea7b7028963d704
Zachary <zacharylampi96@gmail.com>
2022-05-18 10:04:18 -0400

    commit tree ccd8a1ef7024bac7d65b3d0933298cc0877b0a87

---
+++ index.js
@@ -0,0 +1,9 @@
+function introductionWithLanguageOptional(name, language="JavaScript") 
+{ return (`Hi, my name is ${name} and I am learning to program in ${language}.`)}
+
+function introductionWithLanguage(name, language="JavaScript") 
+{ return (`Hi, my name is ${name} and I am learning to program in ${language}.`)}
+
+
+function introduction(name) 
+{ return (`Hi, my name is ${name}.`)}
